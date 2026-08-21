997fda9a13f94811e6f48453b542afba96e68a0a
harsh-borse <harshborse202372@gmail.com>
2026-08-21 19:29:41 +0530

    ✅ CLGOPJP55 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/CLGOPJP55/solution.cpp
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 14;
+        int b = 5;
+        // Update your code below this line
+        float result = (float)a/b;
+        System.out.println(result);
+    }
+}

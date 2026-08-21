4ab6d65cb2cb7de5758b1fcc1b69cbbdeee7e265
harsh-borse <harshborse202372@gmail.com>
2026-08-21 19:26:13 +0530

    ✅ CLGOPJP51 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

--- codechef/medium/CLGOPJP51/solution.java
+++ codechef/medium/CLGOPJP51/solution.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         char name = 'D';
         // Update the blank in code below
-        System.out.println(name == 'A' ? 1 : 0); // Use single quotes for individual characters
+        System.out.println(name == 'A' || name == 'C' ? 1 : 0); // Use single quotes for individual characters
     }
 }

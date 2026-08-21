96f3ec869ff1af774413f45aa2b7091d392ac7b5
harsh-borse <harshborse202372@gmail.com>
2026-08-21 18:59:25 +0530

    ✅ CLGOPJP46 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

--- codechef/medium/CLGOPJP46/solution.java
+++ codechef/medium/CLGOPJP46/solution.java
@@ -3,6 +3,6 @@ public class Main {
         int num = 15;
         // Update the blank in code below
 
-        System.out.println((num % 2 == 1) ? 0 : 1 );
+        System.out.println((num % 2 == 0) ? 1 : 0 );
     }
 }

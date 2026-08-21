0a971919af7f6164e5a8203c00365a6005525170
harsh-borse <harshborse202372@gmail.com>
2026-08-21 18:34:52 +0530

    ✅ CLGOPJP35 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/CLGOPJP35/solution.cpp
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = -50;
+		int b = 40;
+		// update your code below this line
+		int sum = a+b;
+		int product = a*b;
+		int quotient = a/b;
+		
+		System.out.println(sum);
+		System.out.println(product);
+		System.out.println(quotient);
+	}
+}

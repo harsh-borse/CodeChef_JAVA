464212fbecab33cf5e6e478693a5873f78392f5c
harsh-borse <harshborse202372@gmail.com>
2026-08-21 18:40:32 +0530

    ✅ CLGOPJP40 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/CLGOPJP40/solution.cpp
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    int length = 11;
+	   // Update your code below this line
+	    length = length % 3;
+	   System.out.println(length);
+		
+	}
+}

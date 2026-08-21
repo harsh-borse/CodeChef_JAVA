3cf52100a0e98e0bdb1eea0657c0a2a8b5f9eb58
harsh-borse <harshborse202372@gmail.com>
2026-08-21 18:38:50 +0530

    ✅ CLGOPJP37 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/CLGOPJP37/solution.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    // Update your code below this line
+	    int a = 21;
+	    int b = 40;
+	    
+	    System.out.println(a+b);
+	    
+		
+	}
+}

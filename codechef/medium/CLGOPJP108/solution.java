c4994d109e97a634578d6925bc5a0a2573c46608
harsh-borse <harshborse202372@gmail.com>
2026-08-22 19:38:42 +0530

    ✅ CLGOPJP108 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP108/solution.java
@@ -0,0 +1,18 @@
+// Debug the given code
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		char[] word = "Ocygen".toCharArray();
+        word[1] = 'x';
+        System.out.println(word);
+	}
+}
+
+/*Note:
+
+The java string toCharArray() method converts this string into character array. It returns a newly created character array, its length is similar to this string and its contents are initialized with the characters of this string.
+
+*/

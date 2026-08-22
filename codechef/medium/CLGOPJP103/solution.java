c3f0383a07d04433d7da08dfac0ebcd806fd6610
harsh-borse <harshborse202372@gmail.com>
2026-08-22 19:28:51 +0530

    ✅ CLGOPJP103 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

--- codechef/medium/CLGOPJP103/solution.java
+++ codechef/medium/CLGOPJP103/solution.java
@@ -1,21 +1,19 @@
+// Debug the code
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char name = 'A';
+		if(name=='A' || name =='C'){
+		    System.out.println(1);
+		}
+		else{
+		    System.out.println(0);
+		}
+		
 
-class Codechef{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.next(); // Input string
-        scanner.close();
-
-        if (s.length() >= 6) {
-            
-            char c1 = s.charAt(0); 
-            char c2 = s.charAt(3);
-            char c3 = s.charAt(5);
-
-            System.out.println("" + c1 + c2 + c3);
-        } else {
-            // Handle the case when indices are out of bounds
-            System.out.println("Invalid indices"); 
-        }
-    }
+	}
 }

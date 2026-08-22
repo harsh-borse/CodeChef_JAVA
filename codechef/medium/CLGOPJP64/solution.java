50ce70abbf0d094d45d5ac0616f06ff5f7a62b7b
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:04:56 +0530

    ✅ CLGOPJP64 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP64/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Update the blank in the code below
+		Scanner sc = new Scanner(System.in);
+		String x = sc.nextLine();
+		System.out.println("Hello " + x);
+	}
+}

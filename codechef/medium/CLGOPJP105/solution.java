6e18f80073c1603ead48b815173867184dad7b96
harsh-borse <harshborse202372@gmail.com>
2026-08-22 19:31:52 +0530

    ✅ CLGOPJP105 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP105/solution.java
@@ -0,0 +1,10 @@
+// Debug the given code
+public class Main {
+    public static void main(String[] args) {
+        
+        char[] var = "CodeChef".toCharArray();
+        System.out.print(var[0]);
+        System.out.print(var[1]);
+        System.out.print(var[2]);
+    }
+}

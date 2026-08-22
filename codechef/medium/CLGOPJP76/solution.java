300de3dd5eba0fbb0a5ef8d19e0aaa3b05584fbe
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:30:10 +0530

    ✅ CLGOPJP76 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP76/solution.java
@@ -0,0 +1,25 @@
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        // Update the blank in the code given below
+        int b = 23;
+        int r = 45;
+        if(r > b){
+            System.out.println("Rob scored higher marks than Bob.");
+        }
+        else if(r == b){
+            System.out.println("Bob & Rob both scored the same");
+        }
+        
+         r= 15;
+         b= 15;
+        if(r > b){
+            System.out.println("Rob scored higher marks than Bob.");
+        }
+        else if(r == b){
+            System.out.println("Bob & Rob both scored the same");
+        }
+        
+    }
+}

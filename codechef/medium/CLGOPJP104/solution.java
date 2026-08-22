4855fb234ff39904db1efc209ed318fdfa055cb9
harsh-borse <harshborse202372@gmail.com>
2026-08-22 19:31:09 +0530

    ✅ CLGOPJP104 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

--- codechef/medium/CLGOPJP104/solution.java
+++ codechef/medium/CLGOPJP104/solution.java
@@ -1,19 +1,18 @@
-// Debug the code
+// Debug the given code
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		char name = 'C';
-		if(name=='A' || name =='C'){
-		    System.out.println(1);
-		}
-		else{
-		    System.out.println(0);
-		}
-		
-
+	    int age = 18;
+	    if(age >= 18){
+	        System.out.println(1);
+	    }
+	    else{
+	        System.out.println(0);
+	    }
 	}
 }

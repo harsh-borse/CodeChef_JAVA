69084f321e5eabd9e949e41864255919b3339e43
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:31:22 +0530

    ✅ CLGOPJP76 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

--- codechef/medium/CLGOPJP76/solution.java
+++ codechef/medium/CLGOPJP76/solution.java
@@ -1,25 +1,30 @@
+import java.util.*;
+
 class Codechef
 {
-    public static void main (String[] args)
-    {
-        // Update the blank in the code given below
-        int b = 23;
-        int r = 45;
-        if(r > b){
-            System.out.println("Rob scored higher marks than Bob.");
+	public static void main (String[] args)
+	{
+	   // Update the blank in the code given below
+	    int r;
+        int k;
+        r = 24;
+        k = 32;
+        if (r > k) {
+            System.out.println("Ram is heavier than Karan.");
+        } else if (r < k) {
+            System.out.println("Karan is heavier than Ram");
+        } else {
+            System.out.println("Ram & Karan have the same weight!");
         }
-        else if(r == b){
-            System.out.println("Bob & Rob both scored the same");
+
+        r = 78;
+        k = 78;
+        if (r > k) {
+            System.out.println("Ram is heavier than Karan.");
+        } else if (r < k) {
+            System.out.println("Karan is heavier than Ram");
+        } else {
+            System.out.println("Ram & Karan have the same weight!");
         }
-        
-         r= 15;
-         b= 15;
-        if(r > b){
-            System.out.println("Rob scored higher marks than Bob.");
-        }
-        else if(r == b){
-            System.out.println("Bob & Rob both scored the same");
-        }
-        
-    }
+	}
 }

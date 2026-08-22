809e46c32e0a5e12740b71dfddb2c9ece7bdd298
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:26:41 +0530

    ✅ CLGOPJP75 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP75/solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        // 	Your code goes here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println("Coding is Fun!");
+        }
+        else {
+            System.out.println("");
+        }
+
+
+
+    }
+}

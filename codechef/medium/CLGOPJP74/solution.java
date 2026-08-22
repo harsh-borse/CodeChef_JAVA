93fecba3edad3aef83d8b7a09fb1fbd9057aaefa
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:26:24 +0530

    ✅ CLGOPJP74 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

--- codechef/medium/CLGOPJP74/solution.java
+++ codechef/medium/CLGOPJP74/solution.java
@@ -1,17 +1,22 @@
+import java.util.Scanner;
+
 class Codechef
 {
-	public static void main (String[] args)
-	{
-	   // Debug the code given below
-	    int age = 25;
-        int voting_age = 18;
+    public static void main(String[] args)
+    {
+        // 	Your code goes here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        if (age < voting_age){
-            System.out.println("Not old enough to vote.");
-            System.out.println("Wait for " + (voting_age - age) + " years");
-
-        }else {
-            System.out.println("Old enough to vote!");
+        if (a > b) {
+            System.out.println("Coding is Fun!");
         }
-	}
+        else {
+            System.out.println("");
+        }
+
+
+
+    }
 }

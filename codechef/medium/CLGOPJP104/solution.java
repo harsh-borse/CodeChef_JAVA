9bdb034be4fe42bae4434fe0cc903ca7a991cd64
harsh-borse <harshborse202372@gmail.com>
2026-08-22 19:29:44 +0530

    ✅ CLGOPJP104 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP104/solution.java
@@ -0,0 +1,19 @@
+// Debug the code
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char name = 'C';
+		if(name=='A' || name =='C'){
+		    System.out.println(1);
+		}
+		else{
+		    System.out.println(0);
+		}
+		
+
+	}
+}

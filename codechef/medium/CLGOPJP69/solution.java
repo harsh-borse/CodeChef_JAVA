ffba7b01628a9fe0b265e9dce743d7021a2d3e7c
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:12:44 +0530

    ✅ CLGOPJP69 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP69/solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Update the blank in the code given below
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println("Square is: " + (num * num));
+		System.out.println("Cube is: " + (num * num * num));
+	}
+}

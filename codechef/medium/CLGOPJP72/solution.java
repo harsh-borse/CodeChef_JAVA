7d2bf333bbec486b99137446dcb66686df32ee02
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:19:07 +0530

    ✅ CLGOPJP72 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP72/solution.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	   // Update the blank in the code given below
+		int Age = 25;
+        int Vage = 18;
+
+        if (Age >= Vage) {
+            System.out.println("Old enough to vote!");
+        } else {
+            System.out.println("Not old enough to vote.");
+        }
+	}
+}

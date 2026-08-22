bb223f9504d03b30af9b6a79680e27028508f462
harsh-borse <harshborse202372@gmail.com>
2026-08-22 19:16:00 +0530

    ✅ CLGOPJP96 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP96/solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+    public static void main(String[] args) {
+        int a = 3; // change capital I to small i
+        int b = 5;
+        System.out.println(a * b) ;  // add semicolon
+    }
+}

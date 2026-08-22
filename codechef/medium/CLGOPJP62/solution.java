d1be3ccc190c14f831f2650a39efbb41b7e9475d
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:04:03 +0530

    ✅ CLGOPJP62 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP62/solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Update the blank in the code below
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt() ;
+		int b = sc.nextInt();
+		int sum = a + b;
+		int diff = a - b;
+		System.out.println("Sum is: " + sum);
+		System.out.println("Difference is: " + diff);
+	}
+}

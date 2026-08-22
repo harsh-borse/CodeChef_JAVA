07139355d4ef4756d37b051b2528d8e0b1f6b5fc
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:43:57 +0530

    ✅ CLGOPJP85 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP85/solution.java
@@ -0,0 +1,24 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	   // Update the blank in the code given below
+		int a = 15;
+        
+        if (a % 7 == 0 && a % 5 == 0) {
+            System.out.println("The number is divisible by both 5 & 7");
+        }
+            else {
+            System.out.println("The number is not divisible by both 5 & 7");
+        }
+        
+        a = 35;
+        
+        if (a % 7 == 0 && a % 5 == 0) {
+            System.out.println("The number is divisible by both 5 & 7");
+        }
+        else {
+            System.out.println("The number is not divisible by both 5 & 7");
+        }
+	}
+}

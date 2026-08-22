3c740a25ac45f688243c66ccb6467b8552f13d35
harsh-borse <harshborse202372@gmail.com>
2026-08-22 19:00:43 +0530

    ✅ CLGOPJP90 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP90/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        // Complete the code
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        if( x < 70){
+            System.out.println("Order Confirmed");
+        }else{
+            System.out.println("Order Limit reached");
+        }
+        
+        System.out.println("Thank YOU!");
+        
+            
+        
+
+    }
+}

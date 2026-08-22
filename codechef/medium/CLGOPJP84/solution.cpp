72de9499b3cda45344d0cf834f586d823ecd236c
harsh-borse <harshborse202372@gmail.com>
2026-08-22 18:41:05 +0530

    ✅ CLGOPJP84 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/CLGOPJP84/solution.cpp
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	   // Update the blank in the code given below
+	    Scanner read= new Scanner(System.in);
+
+        int a = read.nextInt();
+        int b = read.nextInt();
+        int c = read.nextInt();
+        if (a >= b && a>=c ) {
+            System.out.println("Bravo!");
+        } else {
+            System.out.println("Try Again");
+        }
+
+        a = read.nextInt();
+        b = read.nextInt();
+        c = read.nextInt();
+        if (a >= b && a>=c ) {
+            System.out.println("Bravo!");
+        } else {
+            System.out.println("Try Again");
+        }
+	}
+}

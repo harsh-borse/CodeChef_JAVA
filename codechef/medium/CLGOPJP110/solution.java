e1e58c3dd9cf0c2c3540d0c6bf6d3d2a6d8489f1
harsh-borse <harshborse202372@gmail.com>
2026-08-23 12:22:25 +0530

    ✅ CLGOPJP110 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ codechef/medium/CLGOPJP110/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int C;
+        C = scanner.nextInt();
+        if (C > 20) {
+            System.out.println("HOT");
+        } else {
+            System.out.println("COLD");
+        }
+        scanner.close();
+    }
+}

ab37cf0f9c2d2818c10991463e0ae80f10eb8cb2
HARRY <harshborse202372@gmail.com>
2026-08-24 19:09:28 +0530

    ✅ CLGOPJP120 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/CLGOPJP120/solution.java
+++ codechef/medium/CLGOPJP120/solution.java
@@ -1,20 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Codechef
 {
-	public static void main (String[] args)
+	public static void main (String[] args) 
 	{
-		Scanner read = new Scanner(System.in);
-		int num = read.nextInt();
-		int a = 0;
-		
-		while( a < num){
-		    System.out.println(a);
-		    a++;
-		    
-		}
-		// Update your code below this line
-
-
+		// your code goes here
+        int i = 2;
+        while( i <= 128){
+            
+            System.out.println(i);
+            i = i*2;
+        
+        }
+        
 	}
 }

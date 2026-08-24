19ab4f14e2b42f7f8d156fc414f4275607abfe07
harsh-borse <harshborse202372@gmail.com>
2026-08-24 18:38:32 +0530

    ✅ CLGOPJP115 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/CLGOPJP115/solution.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = 0;
+		
+		while(a < 7){
+		    System.out.println(a);
+		    a++;
+		}
+        // Write the loop here
+        
+	}
+}

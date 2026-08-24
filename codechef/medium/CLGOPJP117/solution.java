6543c4cd5ea5e5b08e754fd6b19e529e6ffa6047
harsh-borse <harshborse202372@gmail.com>
2026-08-24 18:40:43 +0530

    ✅ CLGOPJP117 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/CLGOPJP117/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    int i = 5; // Initialization
+		while (i <= 20) { // Condition
+            System.out.println(i);
+            i++; // Update statement
+        }
+
+	}
+}

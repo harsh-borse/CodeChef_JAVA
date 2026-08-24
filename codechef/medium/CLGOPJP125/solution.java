9368be1708d327c029c88de3ca429b7bee119a19
HARRY <harshborse202372@gmail.com>
2026-08-24 19:57:34 +0530

    ✅ CLGOPJP125 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/CLGOPJP125/solution.java
@@ -0,0 +1,21 @@
+// Debug the code below to solve the problem.
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+
+        int N, sum = 0;
+        int i = 1 ;
+    
+        N = scanner.nextInt();
+
+        while ( i <= N) {
+            sum = sum + i;
+            i++;
+        }
+        System.out.println(sum);
+	}
+}

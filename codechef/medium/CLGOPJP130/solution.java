4672a9c486a79b3a17a5d4c1fc574c94611194e6
HARRY <harshborse202372@gmail.com>
2026-08-25 21:59:33 +0530

    ✅ CLGOPJP130 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

--- codechef/medium/CLGOPJP130/solution.java
+++ codechef/medium/CLGOPJP130/solution.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 class Codechef
 {
-	public static void main (String[] args)
-	{
-		Scanner read = new Scanner(System.in);
-		int num = read.nextInt();
-		int fact = 1;
-		for(int i = 1; i <= num; i++){
-		    fact = fact * i;
-		    
-		    
-		}
-		    System.out.println(fact);
-		//  Write your code here
-		
-	}
+    public static void main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int num = read.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+
+
+        }
+        System.out.println(fact);
+        //  Write your code here
+
+    }
 }

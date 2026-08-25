8c233571f8383b83d08a91b2013e5001bd3d76f9
HARRY <harshborse202372@gmail.com>
2026-08-25 21:50:35 +0530

    ✅ CLGOPJP128 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/CLGOPJP128/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Upate the blank given below.
+		Scanner read = new Scanner(System.in);
+		int n = read.nextInt();
+		for(int i = 0; i < n; i++){
+		    System.out.println("Java");
+		}
+	}
+}

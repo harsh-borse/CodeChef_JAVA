403c37ecf3a55af9207554c28bd165f935fe993c
HARRY <harshborse202372@gmail.com>
2026-08-25 21:46:30 +0530

    ✅ CLGOPJP127 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

--- codechef/medium/CLGOPJP127/solution.java
+++ codechef/medium/CLGOPJP127/solution.java
@@ -5,16 +5,23 @@ class Codechef
 	public static void main (String[] args) 
 	{
 		Scanner scanner = new Scanner(System.in);
-        int num, count = 0;
-        num = scanner.nextInt();
 
-        while (num != 0) {
-            num = num / 10;
-            count++;
-            
+        int n = scanner.nextInt();
+        int i = 2;
+        boolean isPrime = true; // Assum n is prime.
+
+        while (i < n) {
+           if( n % i == 0){
+               isPrime = false;
+           } 
+            i++;
             // Update your code below this line.
             
         }
-        System.out.println(count);
+        if (isPrime) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
 	}
 }

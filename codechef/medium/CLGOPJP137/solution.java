119d910ecf4d47cc2538d70d8cd2f1cf12de03a8
HARRY <harshborse202372@gmail.com>
2026-08-26 23:10:11 +0530

    ✅ CLGOPJP137 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/CLGOPJP137/solution.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		//  Replace the underscores(__) and complete the code
+		for (int i = 0; i <= 15; i++) {
+            if (i == 13) {
+                break;
+            }
+            System.out.println(i);
+        }
+	}
+}

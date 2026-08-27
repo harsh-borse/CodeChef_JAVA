33dc05a9ca85a313eed14fc5a90e42eb196403d2
HARRY <harshborse202372@gmail.com>
2026-08-27 23:07:57 +0530

    ✅ CLGOPJP142 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

--- codechef/medium/CLGOPJP142/solution.java
+++ codechef/medium/CLGOPJP142/solution.java
@@ -1,18 +1,22 @@
+// Update the code below to solve the problem.
 import java.util.Scanner;
+
 class Codechef
 {
 	public static void main (String[] args)
 	{
-        Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
-        for (int i = 1; i <= n; i++) 
+        
+        for(int i=1 ; i <=n ; i++)
         {
-            for (int j = 1; j <= i; j++) 
+            for(int j=1; j<= n; j++)
             {
                 System.out.print("* ");
+                
             }
             System.out.println();
         }
+
 	}
 }

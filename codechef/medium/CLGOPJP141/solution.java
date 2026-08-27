012ad69ab05056eb4926f9fa1e572b1c9cceec32
HARRY <harshborse202372@gmail.com>
2026-08-27 22:58:52 +0530

    ✅ CLGOPJP141 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

--- codechef/medium/CLGOPJP141/solution.java
+++ codechef/medium/CLGOPJP141/solution.java
@@ -10,7 +10,7 @@ class Codechef
         {
             for (int j = 1; j <= i; j++) 
             {
-                System.out.println("* ");
+                System.out.print("* ");
             }
             System.out.println();
         }

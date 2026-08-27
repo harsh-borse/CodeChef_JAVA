c2be2dfac4d76a48c1ffc7446b8b06c9a7cf4914
HARRY <harshborse202372@gmail.com>
2026-08-27 23:33:21 +0530

    ✅ CLGOPJP144 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/CLGOPJP144/solution.java
@@ -0,0 +1,28 @@
+// Update the code below to solve the problem.
+import java.util.Scanner;
+
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j = 1; j<= N - i; j++)
+            {
+                System.out.print(" ");
+                
+            }
+            for(int j = 1; j<= 2*i - 1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Update the code below this line.
+
+	}
+}
